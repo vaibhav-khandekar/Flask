7533cb32c4f481a17f2ded7d1480784bbc437c40
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-01-19 10:24:52 +0530

    template1.py file added

---
+++ Template/template-variables/template1.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    sample_variable = 'Vaibhav'
+    letters = list(sample_variable)
+    dictionary = {'name':'Vaibhav Khandekar','location':'Mumbai'}
+    return render_template('index.html',my_variable = sample_variable, letters = letters, dictionary = dictionary)
+
+if __name__ == '__main__':
+    app.run(debug = True)
